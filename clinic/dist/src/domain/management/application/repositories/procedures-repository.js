d3cda830b5a9d393eeb7de343a32a5681b38b301
otavioCollere <otaviosk59@gmail.com>
2025-08-14 10:33:44 -0300

    fix : new folder structure for CI/CD bug

--- clinic/dist/src/domain/management/application/repositories/procedures-repository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProceduresRepository = void 0;
-class ProceduresRepository {
-}
-exports.ProceduresRepository = ProceduresRepository;
-//# sourceMappingURL=procedures-repository.js.map
--- clinic/dist/src/infra/http/controllers/appointments/reschedule-appointment.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=reschedule-appointment.js.map
